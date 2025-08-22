b933b5d66295e026511af57eb65741c69d6d4c5e
Jayant Seth <jayant.seth94@outlook.com>
2025-08-22 08:10:22 +0530

    Added a basic report blueprint

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+      content: [
+        "./*.html", // Adjust this path to include your HTML files
+      ],
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+    };
